34060008c56dd389cb58d3dce51b894705c1e90b
mhelvens <mhelvens@gmail.com>
2016-08-12 23:02:20 +0100

    added succeeding test to disprove bug

--- test/modules/lyphs.spec.js
+++ test/modules/lyphs.spec.js
@@ -61,7 +61,14 @@ describe("'lyphs' Module", () => {
 		expect(cylindricalLyph.outerBorder).to.be.an.instanceof(Border);
 		expect(cylindricalLyph.minusBorder).to.be.an.instanceof(Border);
 		expect(cylindricalLyph.plusBorder ).to.be.an.instanceof(Border);
-
+		
+		expect(new Set([
+			cylindricalLyph.innerBorder,
+			cylindricalLyph.outerBorder,
+			cylindricalLyph.minusBorder,
+			cylindricalLyph.plusBorder
+		]).size).to.equal(4);
+		
 	});
 	
 	it("exports classes that have the properties, relationships and relationshipShortcuts of their superclasses", () => {
@@ -72,6 +79,34 @@ describe("'lyphs' Module", () => {
 		expect(Lyph.relationships        ).to.have.property('-->ContainsMaterial');
 		expect(Lyph.relationshipShortcuts).to.have.property('materials');
 		
-	})
+	});
+	
+	it("(regression test: auto-synchronized border-natures?)", async () => {
+		
+		const {
+			CylindricalLyph,
+			Border
+		} = module.classes;
+		
+		let cylindricalLyph = CylindricalLyph.new();
+		
+		expect(new Set([
+			cylindricalLyph.innerBorder,
+			cylindricalLyph.outerBorder,
+			cylindricalLyph.minusBorder,
+			cylindricalLyph.plusBorder
+		]).size).to.equal(4);
+		
+		expect(cylindricalLyph.innerBorder.nature).to.eql(['open', 'closed']);
+		
+		cylindricalLyph.innerBorder.nature = ['open'];
+		
+		expect(cylindricalLyph.innerBorder.nature).to.eql(['open']);
+		
+		cylindricalLyph.outerBorder.nature = ['closed']; // setting outerBorder nature
+		
+		expect(cylindricalLyph.innerBorder.nature).to.eql(['open']);
+		
+	});
 	
 });
