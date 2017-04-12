967a7fb87a5e4e55c232a405fc593d36ed7b0bc2
mhelvens <mhelvens@gmail.com>
2017-04-12 12:01:01 +0100

    cross-platform compatible test configuration

--- webpack.config-test.js
+++ webpack.config-test.js
@@ -10,7 +10,7 @@ module.exports = {
 				loader: 'babel-loader'
 			},
 			{
-				test: /node_modules\/utilities\/src\/.*\.js$/,
+				test: /node_modules[\/\\]utilities[\/\\]src[\/\\].*\.js$/,
 				loader: 'babel-loader'
 			},
 			{
